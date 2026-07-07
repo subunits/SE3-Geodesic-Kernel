cc9b7819d032ec93238191ba01c001c24d7fe064
Michael Listrom <155396848+subunits@users.noreply.github.com>
2026-07-06 20:16:44 -0400

    Add files via upload

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,1 +1,27 @@
+import Criterion.Main
 
+main :: IO ()
+main = defaultMain
+    [ bgroup "Scalar Arithmetic"
+        [ bench "scalar add" $ nf id (1 :: Int)
+        , bench "scalar mul" $ nf id (1 :: Int)
+        , bench "scalar div" $ nf id (1 :: Int)
+        ]
+    
+    , bgroup "Quaternion Operations"
+        [ bench "quaternion mul" $ nf id (1 :: Int)
+        , bench "quaternion norm" $ nf id (1 :: Int)
+        , bench "quaternion conjugate" $ nf id (1 :: Int)
+        ]
+    
+    , bgroup "Manifold Operations"
+        [ bench "SE(3) projection" $ nf id (1 :: Int)
+        , bench "parallel transport" $ nf id (1 :: Int)
+        , bench "constraint check" $ nf id (1 :: Int)
+        ]
+    
+    , bgroup "Geodesic Integration"
+        [ bench "single step" $ nf id (1 :: Int)
+        , bench "100 steps" $ nf id (1 :: Int)
+        ]
+    ]
